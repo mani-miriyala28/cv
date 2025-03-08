00bc989226d684e438e7b66d273ef652740b0056
mani-miriyala28 <111078616+mani-miriyala28@users.noreply.github.com>
2025-03-08 12:01:32 +0530

    Updated Deployment

--- cv/astro.config.mjs
+++ cv/astro.config.mjs
@@ -1,13 +1,15 @@
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
-
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+    imageService: true,
+  }),
 });
