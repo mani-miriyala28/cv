0db69dfddf7a18a51cc455915b7899ca47bb916f
Mani Babu Miriyala <maniforin1@gmail.com>
2025-03-08 11:21:57 +0530

    updated configuratiins to deploy

--- cv/astro.config.mjs
+++ cv/astro.config.mjs
@@ -2,8 +2,8 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
